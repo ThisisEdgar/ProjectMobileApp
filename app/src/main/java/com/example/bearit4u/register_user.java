7e1cc5e8e4f244718c311d23ee5e52bfff6c80b4
Nacho Trujillo <kellhendros@icloud.com>
2023-03-27 00:23:29 -0700

    Changed the registration to register user Basic Validation for the user form Delay closing on the form implemented

--- app/src/main/java/com/example/bearit4u/register_user.java
+++ app/src/main/java/com/example/bearit4u/register_user.java
@@ -4,30 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class register_user extends AppCompatActivity {
+    DataBaseHelper DB;
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DB = new DataBaseHelper(this);
         setContentView(R.layout.activity_register_user);
-        EditText fn= findViewById(R.id.inputFName_RU);
-        EditText ln=findViewById(R.id.inputLN_RU);
-        EditText address=findViewById(R.id.inputAddress_RU);
-        EditText phone=findViewById(R.id.inputPhone_RU);
-        EditText email=findViewById(R.id.inputEmail_RU);
-        EditText password= findViewById(R.id.inputPass_RU);
-        Button cancel= findViewById(R.id.btncancel_RU);
+        EditText fn = findViewById(R.id.inputFName_RU);
+        EditText ln = findViewById(R.id.inputLN_RU);
+        EditText address = findViewById(R.id.inputAddress_RU);
+        EditText phone = findViewById(R.id.inputPhone_RU);
+        EditText email = findViewById(R.id.inputEmail_RU);
+        EditText password = findViewById(R.id.inputPass_RU);
+        Button cancel = findViewById(R.id.btncancel_RU);
         Button register = findViewById(R.id.btnRegister_RU);
-
-
-
-
-
-
 
 
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -37,10 +37,75 @@ public class register_user extends AppCompatActivity {
             }
         });
         register.setOnClickListener(new View.OnClickListener() {
+
+            // validation
+            public boolean checkData() {
+
+                CharSequence s = fn.getText().toString();
+                CharSequence t = ln.getText().toString();
+                CharSequence r = address.getText().toString();
+                CharSequence i = phone.getText().toString();
+                CharSequence n = email.getText().toString();
+                CharSequence g = password.getText().toString();
+
+                if (TextUtils.isEmpty(s)) {
+
+                    Toast.makeText(register_user.this, "Please add a First Name", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (TextUtils.isEmpty(t)) {
+
+                    Toast.makeText(register_user.this, "Please add a Last Name", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (TextUtils.isEmpty(r)) {
+
+                    Toast.makeText(register_user.this, "Please add a Address", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (TextUtils.isEmpty(i)) {
+
+                    Toast.makeText(register_user.this, "Please add a Phone", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (TextUtils.isEmpty(n)) {
+
+                    Toast.makeText(register_user.this, "Please add a email", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (TextUtils.isEmpty(g)) {
+
+                    Toast.makeText(register_user.this, "Please add a Password", Toast.LENGTH_SHORT).show();
+                    return true;
+                }else
+                return false;
+            }
+
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(register_user.this, menu_user.class);
+
+                if (checkData() == true) {
+
+                    Toast.makeText(register_user.this, "error detected", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    DB.addUserData(
+                            fn.getText().toString().trim(),
+                            ln.getText().toString().trim(),
+                            address.getText().toString().trim(),
+                            phone.getText().toString(),
+                            email.getText().toString().trim(),
+                            password.getText().toString()
+                    );
+
+                    Toast.makeText(register_user.this, "Register success!, you will return to the previous screen", Toast.LENGTH_SHORT).show();
+
+                    handler.postDelayed(new Runnable() {
+                        public void run() {
+                            finish();
+                        }
+                    }, 5* 1000);
+
+                }
             }
         });
     }
+
+
 }
