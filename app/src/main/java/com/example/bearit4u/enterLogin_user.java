a2c6667b2b29e8350861bd9e9eeff0fa725313b2
ThisisEdgar <distinguisheduser@gmail.com>
2023-03-28 17:42:11 -0700

    Passed user ID through various activities. Created database addAppointment method Finished Search or book for service provider fixed CustomAdapterSearch and enterLogin_user

--- app/src/main/java/com/example/bearit4u/enterLogin_user.java
+++ app/src/main/java/com/example/bearit4u/enterLogin_user.java
@@ -24,6 +24,7 @@ public class enterLogin_user extends AppCompatActivity {
         Button button2=findViewById(R.id.btnCancel_ELI);
         EditText editText1= findViewById(R.id.inputUser_ELI);
         EditText editText2 = findViewById(R.id.inputPassword_ELI);
+        //Initialize view User Data
         DataBaseHelper databaseHelper = new DataBaseHelper(this);
         Cursor cursor = databaseHelper.viewUserData();
 
--- app/src/main/java/com/example/bearit4u/searchOrBookUser.java
+++ app/src/main/java/com/example/bearit4u/searchOrBookUser.java
@@ -5,30 +5,79 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+
 
 public class searchOrBookUser extends AppCompatActivity  implements CustomAdapterSearch.ItemClickListener{
-    //erase position in the future
-    Integer[] position;
     Integer pos;
-    String[] city;
+    String[] provider;
+    String day,month,year,selectedCity,selectedProvider,user_id,provider_id;
+    Button btnbook,btngoBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_or_book_user);
 
-        //Intent intent = new Intent(SelectCityandDate.class,searchOrBookUser.this);
+
+        //get the date and the city
+        Intent intent = new Intent(getIntent());
+        if (intent != null)
+        {
+            day = intent.getStringExtra("day");
+            month = intent.getStringExtra("month");
+            year = intent.getStringExtra("year");
+            selectedCity= intent.getStringExtra("city");
+            user_id = intent.getStringExtra("user_id");
+        }
+        //Fill the array cities with all the cities
+        DataBaseHelper databaseHelper = new DataBaseHelper(this);
+        Cursor cursor = databaseHelper.viewProvidersByCity(selectedCity);
+        Integer numberOfarray =0;
+        provider = new String[cursor.getCount()];
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String allCities = cursor.getString(2);
+                provider_id = cursor.getString(0);
+
+                provider[numberOfarray] = allCities;
+                numberOfarray++;
+            }
+        }
         RecyclerView recyclerView = findViewById(R.id.providerList);
-        CustomAdapterSearch customAdapterSearch = new CustomAdapterSearch(this,position,city,this);
+        CustomAdapterSearch customAdapterSearch = new CustomAdapterSearch(this,provider,this);
         Integer numOfCols =1;
         recyclerView.setLayoutManager(new GridLayoutManager(this,numOfCols));
         recyclerView.setAdapter(customAdapterSearch);
+        btnbook = findViewById(R.id.btnBook_SBU);
+        btngoBack = findViewById(R.id.btnBack_SBU);
+        btnbook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+        Intent intent = new Intent(searchOrBookUser.this, pickupOrDropOff.class);
+        intent.putExtra("provider", provider[pos]);
+        intent.putExtra("day", day);
+        intent.putExtra("month", month);
+        intent.putExtra("year", year);
+        intent.putExtra("user_id",user_id);
+        intent.putExtra("provider_id",provider_id);
+        startActivity(intent);
+            }
+        });
+
     }
     // Check this
     @Override
     public void onItemClick(View view, Integer pos) {
+        //Create pickupOrDropOff
+        selectedProvider = provider[pos];
+        Toast.makeText(searchOrBookUser.this, "Selected Provider: "+provider[pos]+ "date: "+month+"/"+day+"/"+year
+                ,Toast.LENGTH_LONG).show();
         this.pos = pos;
     }
+
 }
